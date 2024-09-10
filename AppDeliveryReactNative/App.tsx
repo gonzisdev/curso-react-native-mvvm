d5d233f50887b857622916cc7df3f4df5ae0b051
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-10 11:47:13 +0200

    update main screen design

--- AppDeliveryReactNative/App.tsx
+++ AppDeliveryReactNative/App.tsx
@@ -8,6 +8,13 @@ export default function App() {
         source={require("./assets/chef.jpg")} 
         style={styles.imageBackground}
       />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.logoImage}
+        />
+        <Text style={styles.logoText}>FOOD APP</Text>
+      </View>
       <View style={styles.form}>
 
       </View>
@@ -34,6 +41,22 @@ const styles = StyleSheet.create({
     bottom: 0,
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40
+  }, 
+  logoContainer: {
+    position: "absolute",
+    alignSelf: "center",
+    top: "15%"
+  },
+  logoImage: {
+    width: 100,
+    height: 100
+  },
+  logoText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10
   }
 })
 
