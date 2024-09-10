b2e85ab789775dbb5485352bb34d746c4d7cfff6
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-10 11:40:22 +0200

    main screen desing

--- AppDeliveryReactNative/App.tsx
+++ AppDeliveryReactNative/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require("./assets/chef.jpg")} 
+        style={styles.imageBackground}
+      />
+      <View style={styles.form}>
+
+      </View>
     </View>
   );
 }
@@ -13,9 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
   },
+  imageBackground: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.7,
+    bottom: "30%"
+  },
+  form: {
+    width: "100%",
+    height: "40%",
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40
+  }
 })
 
