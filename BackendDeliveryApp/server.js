988a2956b173f429736601d29a05cc6b735131ca
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-11 12:27:57 +0200

    fix cors dev

--- BackendDeliveryApp/server.js
+++ BackendDeliveryApp/server.js
@@ -14,7 +14,7 @@ dotenv.config()
 const whitelist = [process.env.FRONTEND_URL]
 const corsOptions = {
     origin: function(origin, callback){
-        if(!whitelist.includes(origin)){
+        if(!origin || whitelist.includes(origin)){
             // Puede consultar la API
             callback(null, true)
         }else{
