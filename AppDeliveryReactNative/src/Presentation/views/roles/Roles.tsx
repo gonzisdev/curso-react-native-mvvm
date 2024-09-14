36d3871321b24baf358ca86e284ea23f1fe80c5b
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-14 18:26:47 +0200

    carousel

--- AppDeliveryReactNative/src/Presentation/views/roles/Roles.tsx
+++ AppDeliveryReactNative/src/Presentation/views/roles/Roles.tsx
@@ -1,20 +1,44 @@
-import { View, Text, FlatList, Dimensions } from "react-native"
-import useViewModel from "./ViewModel"
-import styles from "./Styles"
+import { useState } from "react"
+import { View, Dimensions } from "react-native"
 import { RolesItem } from "./Item"
+import Carousel from "react-native-reanimated-carousel"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import useViewModel from "./ViewModel"
 
 export const RolesScreen = () => {
 
     const { user } = useViewModel()
+
     const width = Dimensions.get('window').width
+    const height = Dimensions.get('window').height
+
+    const [mode, setMode] = useState<any>('horizontal-stack')
+    const [snapDirection, setSnapDirection] = useState<'left' | 'right'>('left')
 
   return (
-    <View>
-        <FlatList
-            data={user?.roles}
-            renderItem={({item}) => <RolesItem rol={item} height={420} width={width - 100} />}
-            keyExtractor={(item) => item.id} 
-        />
-    </View>
+    <GestureHandlerRootView style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <View>
+            {/* <FlatList
+                data={user?.roles}
+                renderItem={({item}) => <RolesItem rol={item} height={420} width={width - 100} />}
+                keyExtractor={(item) => item.id} 
+            /> */}
+            <Carousel
+                loop={false}
+                width={width}
+                height={height / 2}
+                autoPlay={false}
+                data={user?.roles!}
+                scrollAnimationDuration={5000}
+                // onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ item }) => <RolesItem rol={item} height={420} width={width - 100} />}
+                modeConfig={{
+                    snapDirection,
+                    stackInterval: 30
+                }}
+                mode={mode}
+            />
+        </View>
+    </GestureHandlerRootView>
   )
 }
--- AppDeliveryReactNative/src/Presentation/views/roles/Styles.ts
+++
@@ -1,5 +0,0 @@
-import { StyleSheet } from "react-native"
-
-const RolesStyles = StyleSheet.create({})
-
-export default RolesStyles
