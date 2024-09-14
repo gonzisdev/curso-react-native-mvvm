896021a7af01e42e4857d9feea142204d45d3135
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-14 18:10:45 +0200

    role cards styles

--- AppDeliveryReactNative/src/Presentation/views/roles/Roles.tsx
+++ AppDeliveryReactNative/src/Presentation/views/roles/Roles.tsx
@@ -1,16 +1,18 @@
-import { View, Text, FlatList } from "react-native"
+import { View, Text, FlatList, Dimensions } from "react-native"
 import useViewModel from "./ViewModel"
 import styles from "./Styles"
+import { RolesItem } from "./Item"
 
 export const RolesScreen = () => {
 
     const { user } = useViewModel()
+    const width = Dimensions.get('window').width
 
   return (
     <View>
         <FlatList
             data={user?.roles}
-            renderItem={({item}) => <Text>{item.name}</Text>}
+            renderItem={({item}) => <RolesItem rol={item} height={420} width={width - 100} />}
             keyExtractor={(item) => item.id} 
         />
     </View>
