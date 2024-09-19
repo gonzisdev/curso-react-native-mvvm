1b2c18fc55161b5164f54725ce3fa57a61ab0758
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-19 12:41:39 +0200

    components and styles map

--- AppDeliveryReactNative/src/Presentation/views/client/address/map/AddressMap.tsx
+++ AppDeliveryReactNative/src/Presentation/views/client/address/map/AddressMap.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react"
-import { ToastAndroid, View } from "react-native"
+import { Image, ToastAndroid, View, Text } from "react-native"
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps"
+import { RoundedButton } from "../../../../components/RoundedButton"
 import useViewModel from "./ViewModel"
+import stylesMap from "./StylesMap"
 import styles from "./Styles"
 
 export const ClientAddressMapScreen = () => {
@@ -18,9 +20,20 @@ export const ClientAddressMapScreen = () => {
     <View style={styles.container}>
         <MapView 
             ref={mapRef}
+            customMapStyle={stylesMap}
             style={{height: "100%", width: "100%"}}
             provider={PROVIDER_GOOGLE}
         />
+        <Image 
+            style={styles.imageLocation}
+            source={require('../../../../../../assets/location_home.png')}
+        />
+        <View style={styles.refPoint}>
+            <Text style={styles.refPointText}>Punto de ref</Text>
+        </View>
+        <View style={styles.buttonRefPoint}> 
+            <RoundedButton text="SELECCIONAR PUNTO" onPress={() => {}} />
+        </View>
     </View>
   )
 }
